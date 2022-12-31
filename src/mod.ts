efcf3cd0b392b6e37cd3d5d820164ebc3a11c395
Jonathan Hassel <31740413+jotoh98@users.noreply.github.com>
2022-12-31 22:26:35 +0100

    Update deploy.yml

---
+++ src/mod.ts
@@ -0,0 +1,22 @@
+import {serve} from "https://deno.land/std@0.140.0/http/server.ts";
+
+
+serve(async (r) => {
+    const pathname = new URL(r.url).pathname;
+    const b = pathname.startsWith('/assets');
+    if (b) {
+        const content = await Deno.readTextFile(`.${pathname}`);
+        const contentType = pathname.endsWith('.js') ? 'application/javascript' : 'text/css';
+        return new Response(content, {
+            headers: {
+                "content-type": contentType,
+            }
+        })
+    }
+    const HTML = await Deno.readTextFile("./index.html");
+    return new Response(HTML, {
+        headers: {
+            "content-type": "text/html",
+        },
+    })
+});
